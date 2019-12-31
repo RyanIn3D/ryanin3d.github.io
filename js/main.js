2c0b74d009d4a844e556f531ce72fe2a0e6c58c5
RyanIn3D <ryan.alex.davis@gmail.com>
2019-12-30 22:39:55 -0800

    Menu now hover; z-index navbar; resize fix

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-/* DROPDOWN MENU */
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(e) {
-    if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-    }
-  }
-
-  /* HTML REFERENCING */
+/* HTML REFERENCING */
   function includeHTML() {
     var z, i, elmnt, file, xhttp;
     /* Loop through a collection of all HTML elements: */
